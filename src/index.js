a7e5b58c1480ee1c0b9fe3624016c70ca811152e
Sonahit <grandpajok@gmail.com>
2019-09-18 18:32:50 +0600

    Added mobile styles for nav

--- src/index.js
+++ src/index.js
@@ -1,21 +1,39 @@
 //According to .popup-menu_button scaleY(X);
 import "./assets/css/index.css";
-import "./assets/logo.png";
+import "./assets/images/logo.png";
+import "./assets/images/dining_room.png";
+import "./assets/images/walking.png";
+import "./assets/images/white_room.png";
+import "./assets/images/lock.png";
+import "./assets/images/nav_mob.png";
 
+const imageDir = "";
 let flip = -1;
 
 (function() {
   //Init default listeners
   popupLogic();
   modalShow();
+  sliderListener();
+  mobileListener();
 
   function popupLogic() {
-    const popupBtn = document.querySelector(".popup-menu_button");
-    popupBtn.addEventListener("mousedown", ev => {
-      flip = -flip;
-      ev.target.style.transform = `translate(-50%, -100%) scaleY(${flip})`;
-      popupStatus(flip);
-    });
+    const popupBtn = document.getElementsByClassName("popup-menu_button");
+    for (let i = 0; i < popupBtn.length; i++) {
+      popupBtn[i].addEventListener("mousedown", ev => {
+        flip = -flip;
+        ev.target.style.transform = `translate(-50%, -100%) scaleY(${flip})`;
+        popupStatus(flip, i);
+        const selected = document.querySelector(".main_slider--selection");
+        //Hiding select dots
+        setTimeout(
+          () => {
+            selected.style.zIndex = -selected.style.zIndex || -1;
+          },
+          flip === -1 ? 35 * 10 : 10 * 10
+        );
+      });
+    }
   }
 
   function modalShow() {
@@ -42,10 +60,39 @@ let flip = -1;
       showHideModal();
     });
   }
+
+  function sliderListener() {
+    const normalDotClass = "main_slider_dot--inner";
+    const activeDotClass = `active`;
+    const pages = document.getElementsByClassName(normalDotClass);
+    for (let i = 0; i < pages.length; i++) {
+      const slider = document.querySelector(".main_content_slider");
+      pages[i].addEventListener("mousedown", ev => {
+        const prevActive = document.querySelector(`.${activeDotClass}`);
+        if (ev.target.className === normalDotClass) {
+          prevActive.className = normalDotClass;
+          ev.target.className = `${normalDotClass} ${activeDotClass}`;
+          slider.className = `main_content_slider main_content_slider--${i}`;
+        }
+      });
+    }
+  }
+
+  function mobileListener() {
+    const listener = document.querySelector(".nav_mobile--listener");
+    listener.addEventListener("mousedown", () => {
+      const nav = document.querySelector(".nav_mobile_content--hidden") || document.querySelector(".nav_mobile_content--visible");
+      if (nav.className.includes("hidden")) {
+        nav.className = "nav_mobile_content--visible";
+      } else {
+        nav.className = "nav_mobile_content--hidden";
+      }
+    });
+  }
 })();
 
-function popupStatus(flip) {
-  const popupMenu = document.querySelector(".popup-menu");
+function popupStatus(flip, index) {
+  const popupMenu = document.querySelector(`.popup-menu--${index}`);
   for (let i = 0; i < popupMenu.classList.length; i++) {
     if (popupMenu.classList.contains("hidden") && flip === 1) {
       popupMenu.classList.remove("hidden");
